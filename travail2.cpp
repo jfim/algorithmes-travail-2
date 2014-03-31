e9862d54a2d87701d6fa3b41f8aa5d3cde80d34e
Jean-Francois Im <jfim@jean-francois.im>
2014-03-31 03:24:44 -0400

    Ajout du CPP vide

---
+++ travail2.cpp
@@ -0,0 +1,15 @@
+int fibonacci(int n) {
+	return 0;
+}
+
+int pgcd(int n1, int n2) {
+	return 0;
+}
+
+int maximum(int* tableau, int longueur, int maximumTrouve) {
+	return 0;
+}
+
+int indexDe(int* tableau, int longueur, int chiffreRecherche) {
+	return 0;
+}
